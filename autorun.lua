20e196aa8ca6cdeab65535212c2b57fd7b21ef69
たまさん <58621773+kazu55@users.noreply.github.com>
2021-10-15 17:27:10 +0900

    Create autorun.lua

---
+++ autorun.lua
@@ -0,0 +1,16 @@
+--------------------------------------------requires------------------------------------------------
+
+local term = require("term")
+local text = require("text")
+local com = require('component')
+local gpu = com.gpu
+
+----------------------------------------------------------------------------------------------------
+
+term.clear()
+local width, height = gpu.getResolution()
+local text = "untitledsystem Running..."
+
+gpu.set(width / 2 - #text / 2, height / 2, text)
+
+shell.execute("/UntitledOS/System/sys-start.lua")
