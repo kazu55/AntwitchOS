8a1cc0f05c3040849e637097e25ccbba3f6b135b
たまさん <58621773+kazu55@users.noreply.github.com>
2021-10-17 09:23:42 +0900

    Update autorun.lua

--- autorun.lua
+++ autorun.lua
@@ -5,6 +5,7 @@ local text = require("text")
 local com = require('component')
 local gpu = com.gpu
 
+
 ----------------------------------------------------------------------------------------------------
 
 term.clear()
