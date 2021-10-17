e1b071fca97754ce16323ca8b347159371e53b7f
たまさん <58621773+kazu55@users.noreply.github.com>
2021-10-17 10:01:25 +0900

    Update desktop.lua

--- UntitledOS/System/desktop.lua
+++ UntitledOS/System/desktop.lua
@@ -1,18 +1,30 @@
 GUI = require("GUI")
 local computer = require("computer")
+local thread = require "thread"
+
 
 --------------------------------------------------------------------------------
 
 local workspace = GUI.workspace()
 local panel = workspace:addChild(GUI.panel(1, 1, workspace.width, workspace.height, 0x2D2D2D))
-layout:addChild(GUI.roundedButton(workspace.width, workspace.height, 6, 3, 0xFFFFFF, 0x555555, 0x880000, 0xFFFFFF, "Pinned"))
 
+local filesystemDialog = GUI.addFilesystemDialog(workspace, false, 50, math.floor(workspace.height * 0.8), "Open", "Cancel", "File name", "/")
+filesystemDialog:setMode(GUI.IO_MODE_OPEN, GUI.IO_MODE_FILE)
+filesystemDialog:addExtensionFilter(".lua")
+filesystemDialog.onSubmit = function(path)
+  local proc = thread.create(os.execute, path)
+  proc:detach() -- # detach from current process
+end
+
+panel:addChild(GUI.roundedButton(workspace.width, workspace.height, 6, 3, 0xFFFFFF, 0x555555, 0x880000, 0xFFFFFF, "Run the program")).onTouch = function()
+  
 
 local power = workspace:addChild(GUI.titledWindow(25, 11, 60, 20, "Reboot and Shutdown", true))
 -- Attach an single cell layout to it
 local layout = power:addChild(GUI.layout(1, 2, power.width, power.height - 1, 1, 1))
 -- Add some stuff to layout
 layout:addChild(GUI.roundedButton(2, 18, 30, 3, 0xFFFFFF, 0x555555, 0x880000, 0xFFFFFF, "Shutdown")).onTouch = function()
+    
   GUI.alert("GoodBye")
   computer.shutdown()
 end
