a212b73c9112eb8d42ded7b4564481994ca1283c
たまさん <58621773+kazu55@users.noreply.github.com>
2021-10-17 09:35:38 +0900

    Update desktop.lua

--- UntitledOS/System/desktop.lua
+++ UntitledOS/System/desktop.lua
@@ -1,4 +1,4 @@
-local GUI = require("GUI")
+GUI = require("GUI")
 local computer = require("computer")
 
 --------------------------------------------------------------------------------
@@ -6,23 +6,18 @@ local computer = require("computer")
 local workspace = GUI.workspace()
 workspace:addChild(GUI.panel(1, 1, workspace.width, workspace.height, 0x2D2D2D))
 
--- Add menu object to workspace
-local menu = workspace:addChild(GUI.menu(1, 1, workspace.width, 0xEEEEEE, 0x666666, 0x3366CC, 0xFFFFFF))
--- Add context menu and few items to it
-local contextMenu = menu:addItem("test", 0x0)
-contextMenu:addItem("PowerOFF").onTouch = function()
+local power = workspace:addChild(GUI.titledWindow(25, 11, 60, 20, "Reboot and Shutdown", true))
+-- Attach an single cell layout to it
+local layout = window3:addChild(GUI.layout(1, 2, window3.width, window3.height - 1, 1, 1))
+-- Add some stuff to layout
+layout:addChild(GUI.roundedButton(2, 18, 30, 3, 0xFFFFFF, 0x555555, 0x880000, 0xFFFFFF, "Shutdown")).onTouch = function()
+  GUI.alert("GoodBye")
   computer.shutdown()
 end
-contextMenu:addItem("Reboot").onTouch = function()
+layout:addChild(GUI.roundedButton(2, 18, 30, 3, 0xFFFFFF, 0x555555, 0x880000, 0xFFFFFF, "Reboot")).onTouch = function()
+  GUI.alert("Rebooting Computer")
   computer.shutdown(true)
 end
-contextMenu:addSeparator()
-contextMenu:addItem("Save")
-contextMenu:addItem("Save as")
--- Add whatever you want
-menu:addItem("Edit")
-menu:addItem("View")
-menu:addItem("About")
 
 --------------------------------------------------------------------------------
 
