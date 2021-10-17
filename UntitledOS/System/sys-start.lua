31e754a7ae3b727dae1712248e111aa93fc3ec39
たまさん <58621773+kazu55@users.noreply.github.com>
2021-10-17 09:07:10 +0900

    Update sys-start.lua

--- UntitledOS/System/sys-start.lua
+++ UntitledOS/System/sys-start.lua
@@ -12,7 +12,7 @@ local window3 = workspace:addChild(GUI.titledWindow(50, 22, 60, 20, "Start", tru
 local layout = window3:addChild(GUI.layout(1, 2, window3.width, window3.height - 1, 1, 1))
 -- Add some stuff to layout
 layout:addChild(GUI.roundedButton(2, 18, 30, 3, 0xFFFFFF, 0x555555, 0x880000, 0xFFFFFF, "desktop")).onTouch = function()
-  shell.execute("desktop.lua")
+  shell.execute("./desktop.lua")
 end
 
 ------------------------------------------------------------------------------------------
