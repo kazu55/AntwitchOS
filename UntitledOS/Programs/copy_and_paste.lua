4a8cf006900f55110f01f2857c3c92daab5515fa
たまさん <58621773+kazu55@users.noreply.github.com>
2021-11-06 18:32:10 +0900

    Update copy_and_paste.lua

--- UntitledOS/Programs/copy_and_paste.lua
+++ UntitledOS/Programs/copy_and_paste.lua
@@ -7,15 +7,15 @@ local workspace = GUI.workspace()
 workspace:addChild(GUI.panel(1, 1, workspace.width, workspace.height, 0x2D2D2D))
 
 local filesystemDialogx = GUI.addFilesystemDialog(workspace, false, 50, math.floor(workspace.height * 0.8), "Copy", "Cancel", "File name", "/")
-filesystemDialog:setMode(GUI.IO_MODE_OPEN, GUI.IO_MODE_FILE)
-filesystemDialog.onSubmit = function(pathx)
+filesystemDialogx:setMode(GUI.IO_MODE_OPEN, GUI.IO_MODE_FILE)
+filesystemDialogx.onSubmit = function(pathx)
 end
 
 filesystemDialogx:show()
 
 local filesystemDialogy = GUI.addFilesystemDialog(workspace, false, 50, math.floor(workspace.height * 0.8), "Paste", "Cancel", "File name", "/")
-filesystemDialog:setMode(GUI.IO_MODE_OPEN, GUI.IO_MODE_FILE)
-filesystemDialog.onSubmit = function(pathy)
+filesystemDialogy:setMode(GUI.IO_MODE_OPEN, GUI.IO_MODE_FILE)
+filesystemDialogy.onSubmit = function(pathy)
   os.execute("cp " .. pathx .. " " .. pathy)
 end
 
