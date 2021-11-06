cf0e53128d751899ef54c5f0090a4254e3a7d769
たまさん <58621773+kazu55@users.noreply.github.com>
2021-11-06 18:33:14 +0900

    Update copy_and_paste.lua

--- UntitledOS/Programs/copy_and_paste.lua
+++ UntitledOS/Programs/copy_and_paste.lua
@@ -6,20 +6,19 @@ local os = require("os")
 local workspace = GUI.workspace()
 workspace:addChild(GUI.panel(1, 1, workspace.width, workspace.height, 0x2D2D2D))
 
-local filesystemDialogx = GUI.addFilesystemDialog(workspace, false, 50, math.floor(workspace.height * 0.8), "Copy", "Cancel", "File name", "/")
-filesystemDialogx:setMode(GUI.IO_MODE_OPEN, GUI.IO_MODE_FILE)
-filesystemDialogx.onSubmit = function(pathx)
-end
-
-filesystemDialogx:show()
-
 local filesystemDialogy = GUI.addFilesystemDialog(workspace, false, 50, math.floor(workspace.height * 0.8), "Paste", "Cancel", "File name", "/")
 filesystemDialogy:setMode(GUI.IO_MODE_OPEN, GUI.IO_MODE_FILE)
 filesystemDialogy.onSubmit = function(pathy)
   os.execute("cp " .. pathx .. " " .. pathy)
 end
 
+local filesystemDialogx = GUI.addFilesystemDialog(workspace, false, 50, math.floor(workspace.height * 0.8), "Copy", "Cancel", "File name", "/")
+filesystemDialogx:setMode(GUI.IO_MODE_OPEN, GUI.IO_MODE_FILE)
+filesystemDialogx.onSubmit = function(pathx)
+end
+
 filesystemDialogy:show()
+filesystemDialogx:show()
 
 
 --------------------------------------------------------------------------------
