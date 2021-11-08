97e60440840d5f5cc25fd81a53f758229a9dfb6d
たまさん <58621773+kazu55@users.noreply.github.com>
2021-11-08 16:59:45 +0900

    Update uac.lua

--- lib/uac.lua
+++ lib/uac.lua
@@ -2,7 +2,7 @@ local term = require("term")
 local GUI = require("GUI")
 local io = require("io")
 local os = require("os")
-local rootpasswd = "0000"
+rootpasswd = "0000"
 function run()
   term.clear()
   print("Please enter your password. The initial value is 0000.")
