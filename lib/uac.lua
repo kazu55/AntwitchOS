ab1956a75d6f92f12dad4a8e279e712b9f1e8333
たまさん <58621773+kazu55@users.noreply.github.com>
2021-11-08 16:37:41 +0900

    Create uac.lua

---
+++ lib/uac.lua
@@ -0,0 +1,14 @@
+local term = require("term")
+local GUI = require("GUI")
+local io = require("io")
+local rootpasswd = "0000"
+function uac.run()
+  term.clear()
+  print("Please enter your password. The initial value is 0000.")
+  passinput = io.read()
+  if passinput == rootpasswd then
+    return "true"
+  else
+    return "false"
+  end
+end
