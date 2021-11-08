e57eb20456d2921e33345429d391ee1e46e5e9ed
たまさん <58621773+kazu55@users.noreply.github.com>
2021-11-08 18:24:02 +0900

    Update update.lua

--- inst/update.lua
+++ inst/update.lua
@@ -50,12 +50,12 @@ local properties                = {
     -- Customize localization as you want to
     localization      = {
         -- Specify title of your installer
-        title         = "testOS Installer",
+        title         = "testOS Updater",
         -- Use <currentProgress>, <totalProgress> and <currentFile> text insertions to automatically display their values
         currentFile   = "Downloading \"<currentFile>\"",
         totalProgress = "Total progress: <totalProgress>%",
         -- Comment this lines to automatically close installer window
-        finished1     = "testOS has been successfully installed.",
+        finished1     = "testOS has been successfully updated.",
         finished2     = "Press any key to quit."
     },
     -- Customize color scheme as you want to
