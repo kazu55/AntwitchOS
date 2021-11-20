411798e11681e296db5fba1d387dfe37cff9677b
たまさん <58621773+kazu55@users.noreply.github.com>
2021-11-20 09:56:08 +0900

    Update tautorun.lua

--- tautorun.lua
+++ tautorun.lua
@@ -1,8 +1,18 @@
-shell = require("shell")
-GUI = require("GUI")
-term = require("term")
+local shell = require("shell")
+local GUI = require("GUI")
+local term = require("term")
+local cp = require("computer")
 
 ------------------------------------------------------------------
+
 term.clear()
 GUI.aleat("start Copy.  I don't Power off.")
 shell.execute("cp -fr /UntitledOS/temp/* /UntitledOS")
+shell.execute("wget -f https://raw.githubusercontent.com/kazu55/testos/master/autorun.lua /autorun.lua")
+shell.execute("rm -r /UntitledOS/temp/")
+term.clear()
+GUI.aleat("Copy is finished.")
+
+------------------------------------------------------------------
+
+cp.shutdown(true)
