a979535c5b113c2d65cd086fa5c7b04ee0fd1ba6
たまさん <58621773+kazu55@users.noreply.github.com>
2021-11-23 08:58:00 +0900

    Update desktop.lua

--- UntitledOS/System/desktop.lua
+++ UntitledOS/System/desktop.lua
@@ -21,6 +21,10 @@ contextMenu:addItem("Power Options").onTouch = function()
 	Power.layout:addChild(GUI.framedButton(7, 1, 3, 3, 0xFFFFFF, 0xFFFFFF, 0x880000, 0x880000, "Reboot")).onTouch = function()
 		computer.shutdown(true)
 	end
+	workspace:draw()
+	workspace:start()
+end
+
 contextMenu:addItem("Applications").onTouch = function()
 	local container = GUI.addBackgroundContainer(workspace, true, true, " ")
 	local tree2 = container.layout:addChild(GUI.filesystemTree(34, 2, 30, 41, 0xCCCCCC, 0x3C3C3C, 0x3C3C3C, 0x999999, 0x3C3C3C, 0xE1E1E1, 0xBBBBBB, 0xAAAAAA, 0xBBBBBB, 0x444444, GUI.IO_MODE_BOTH, GUI.IO_MODE_FILE))
