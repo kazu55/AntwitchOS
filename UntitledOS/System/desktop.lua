b59d495aeede6391d2cdfb05848628238876543e
たまさん <58621773+kazu55@users.noreply.github.com>
2021-11-23 08:56:32 +0900

    Update desktop.lua

--- UntitledOS/System/desktop.lua
+++ UntitledOS/System/desktop.lua
@@ -12,7 +12,16 @@ local panel = workspace:addChild(GUI.panel(1, 1, workspace.width, workspace.heig
 -- Add menu object to workspace
 local menu = workspace:addChild(GUI.menu(1, 1, workspace.width, 0xEEEEEE, 0x666666, 0x3366CC, 0xFFFFFF))
 -- Add first item with black color. Attack a callback-function to it
-menu:addItem("Applications", 0x0).onTouch = function()
+local contextMenu = menu:addContextMenu("AntwitchOS")
+contextMenu:addItem("Power Options").onTouch = function()
+	local Power = GUI.addBackgroundContainer(workspace, true, true, " ")
+	Power.layout:addChild(GUI.framedButton(3, 1, 3, 3, 0xFFFFFF, 0xFFFFFF, 0x880000, 0x880000, "Power OFF")).onTouch = function()
+		computer.shutdown()
+	end
+	Power.layout:addChild(GUI.framedButton(7, 1, 3, 3, 0xFFFFFF, 0xFFFFFF, 0x880000, 0x880000, "Reboot")).onTouch = function()
+		computer.shutdown(true)
+	end
+contextMenu:addItem("Applications").onTouch = function()
 	local container = GUI.addBackgroundContainer(workspace, true, true, " ")
 	local tree2 = container.layout:addChild(GUI.filesystemTree(34, 2, 30, 41, 0xCCCCCC, 0x3C3C3C, 0x3C3C3C, 0x999999, 0x3C3C3C, 0xE1E1E1, 0xBBBBBB, 0xAAAAAA, 0xBBBBBB, 0x444444, GUI.IO_MODE_BOTH, GUI.IO_MODE_FILE))
 	tree2:updateFileList()
@@ -21,17 +30,6 @@ menu:addItem("Applications", 0x0).onTouch = function()
 	end
 	workspace:draw()
 	workspace:start()
-end
-
-local power = workspace:addChild(GUI.titledWindow(25, 11, 60, 20, "Reboot and Shutdown", true))
--- Attach an single cell layout to it
-local layout = power:addChild(GUI.layout(1, 2, power.width, power.height - 1, 1, 1))
--- Add some stuff to layout
-layout:addChild(GUI.roundedButton(2, 18, 30, 3, 0xFFFFFF, 0x555555, 0x880000, 0xFFFFFF, "Shutdown")).onTouch = function()
-  computer.shutdown()
-end
-layout:addChild(GUI.roundedButton(2, 18, 30, 3, 0xFFFFFF, 0x555555, 0x880000, 0xFFFFFF, "Reboot")).onTouch = function()
-  computer.shutdown(true)
 end
 
 --------------------------------------------------------------------------------
