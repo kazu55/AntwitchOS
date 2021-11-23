c76e1d28692934d1bc58a4513d785497f0b42dce
たまさん <58621773+kazu55@users.noreply.github.com>
2021-11-23 16:10:56 +0900

    Update power_force.lua

--- power_force.lua
+++ power_force.lua
@@ -6,10 +6,9 @@ local _, _, _, key = event.pull("key_down")
 while true do
   if key == 29 then
     computer.shutdown()
-    os.sleep(0)
   end
   if key == 42 then
     computer.shutdown(true)
-    os.sleep(0)
   end
+  os.sleep(0)
 end
