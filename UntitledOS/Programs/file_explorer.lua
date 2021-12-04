80e7745367c538b83fd9d9333949c312578cf6d5
たまさん <58621773+kazu55@users.noreply.github.com>
2021-12-04 12:19:15 +0900

    Update file_explorer.lua

--- UntitledOS/Programs/file_explorer.lua
+++ UntitledOS/Programs/file_explorer.lua
@@ -2,29 +2,25 @@ local shell = require("shell")
 local io = require("io")
 local os = require("os")
 while true do
-  print("Select the operation you want to perform (ls, cd, edit, mkdir, exit)?")
+  print("Select the operation you want to perform (ls, edit, mkdir, exit)?")
   operation = io.read()
-  if operation == ls then
+  if operation == "ls" then
     print("Which directories to list?")
     list_directories = io.read()
     shell.execute("ls " .. list_directories)
     print("done")
-  elseif operation == cd then
-    print("What directory to move to?")
-    cd_directories = io.read()
-    shell.execute("cd " .. cd_directories)
-    print("done")
-  elseif operation == edit then
+  end
+  if operation == "edit" then
     print("Which file to edit?")
     edit_files = io.read()
     shell.execute("edit " .. edit_files)
-  elseif operation == mkdir then
+  end
+  if operation == "mkdir" then
     print("Please enter the name of the folder.")
     makedir = io.read()
     shell.execute("mkdir " .. makedir)
-  elseif operation == exit then
+  end
+  if operation == "exit" then
     os.exit()
-  else
-    print("Did not enter operation.")
   end
 end
