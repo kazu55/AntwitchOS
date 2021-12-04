02b6822f9534debc244fe5591ccdf562fcdc51af
たまさん <58621773+kazu55@users.noreply.github.com>
2021-12-04 20:08:36 +0900

    Update file_explorer.lua

--- UntitledOS/Programs/file_explorer.lua
+++ UntitledOS/Programs/file_explorer.lua
@@ -1,8 +1,12 @@
 local shell = require("shell")
 local io = require("io")
 local os = require("os")
+local term = require("term")
+
+
 while true do
-  print("Select the operation you want to perform (ls, edit, mkdir, exit)?")
+  term.clear()
+  print("Select the operation you want to perform (ls, edit, mkdir, exit, rm)?")
   operation = io.read()
   if operation == "ls" then
     print("Which directories to list?")
@@ -20,7 +24,13 @@ while true do
     makedir = io.read()
     shell.execute("mkdir " .. makedir)
   end
+  if operation == "rm" then
+    print("Please enter the name of the folder.")
+    removedir = io.read()
+    shell.execute("rm " .. removedir)
+  end
   if operation == "exit" then
     os.exit()
   end
+  os.sleep(0)
 end
