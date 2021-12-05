171ac5529c07cbae050041d90d046a334864d150
たまさん <58621773+kazu55@users.noreply.github.com>
2021-12-05 13:44:21 +0900

    Update sys-start.lua

--- UntitledOS/System/sys-start.lua
+++ UntitledOS/System/sys-start.lua
@@ -2,7 +2,9 @@ local image = require("Image")
 local GUI = require("GUI")
 local shell = require("shell")
 local os = require("os")
+local comp = require("computer")
 
+computer.beep(1000, 0.6)
 ------------------------------------------------------------------------------------------
 
 local workspace = GUI.workspace()
