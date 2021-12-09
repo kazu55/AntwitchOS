3ccd8f56e67e9bf35d6d5bdf4b0c6cd862c76029
たまさん <58621773+kazu55@users.noreply.github.com>
2021-12-09 16:17:05 +0900

    Update setup.lua

--- inst/setup.lua
+++ inst/setup.lua
@@ -27,8 +27,6 @@ local function download(url, path)
   shell.execute("wget -fq " .. url .. " " .. path)
 end
 
-download("https://raw.githubusercontent.com/kazu55/AntwitchOS/master/inst/setup2.lua", "./setup2.lua")
-
 local window3 = GUI.titledWindow(3, 3, 60, 20, "AntwitchOS Setup - Start", true)
 local layout1 = window3:addChild(GUI.layout(1, 2, window3.width, window3.height - 1, 1, 1))
 layout1:addChild(GUI.text(1, 1, 0x666666, "Hello, world. start-button click continue."))
