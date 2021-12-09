7d2870be8b037090a2138dea48b5bb5a5a264fcb
たまさん <58621773+kazu55@users.noreply.github.com>
2021-12-09 15:47:29 +0900

    Update setup.lua

--- inst/setup.lua
+++ inst/setup.lua
@@ -29,7 +29,7 @@ end
 
 local window3 = GUI.titledWindow(3, 3, 60, 20, "AntwitchOS Setup - page 0", true)
 local layout1 = window3:addChild(GUI.layout(1, 2, window3.width, window3.height - 1, 1, 1))
-layout1:addChild(GUI.text(1, 1, 0x666666, "Hello, world, start-button click continue."))
+layout1:addChild(GUI.text(1, 1, 0x666666, "Hello, world. start-button click continue."))
 layout1:addChild(GUI.button(1, 3, 36, 3, 0xB4B4B4, 0xFFFFFF, 0x969696, 0xB4B4B4, "Start")).onTouch = function()
 	window3.remove()
 	term.clear()
