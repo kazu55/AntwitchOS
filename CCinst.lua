9ad68a37918a95dd441ef3756cac1c18bb981db8
たまさん <58621773+kazu55@users.noreply.github.com>
2021-12-18 10:15:20 +0900

    Create CCinst.lua

---
+++ CCinst.lua
@@ -0,0 +1,64 @@
+install = {
+  title = 'AntwitchOS',
+  version = '',
+  author = 'TAMASAN',
+  description = 'description'
+  license = 'license'
+  copyrightYear = 2021,
+  copyrightHolders = 'TAMASAN',
+  diskspace = 380000,
+  rebootAfter = true,
+  gitRepo = 'kazu55/AntwitchOS',
+  gitBranch = 'ComputerCraft',
+  branches = {
+    { branch = 'ComputerCraft', description = '1.8+' },
+  },
+  preCopy = function(mode)
+    if mode == 'update' then
+      fs.delete('sys')
+    end
+  end,
+  steps = {
+    install = {
+      'splash',
+      'license',
+      'branch',
+      'files',
+      'review',
+      'install',
+    },
+    update = {
+      'branch',
+      'review',
+      'install',
+    },
+    automatic = {
+      'install',
+    },
+    uninstall = {
+      'review',
+      'uninstall',
+    },
+  },
+}
+
+print('Downloading Installer...')
+
+local url ='https://raw.githubusercontent.com/kepler155c/opus-installer/master/sys/apps/Installer.lua'
+local h = _G.http.get(url)
+if not h then
+  error('Failed to download installer')
+end
+
+local contents = h.readAll()
+if not contents then
+  error('Failed to download installer')
+end
+
+local fn, msg = load(contents, 'Installer.lua', nil, _ENV)
+if not fn then
+  _G.printError(msg)
+else
+  local args = { ... }
+  fn(args[1])
+end
