ec2bf3885d7ebfbe7742231d41e9c5cc637b16e4
たまさん <58621773+kazu55@users.noreply.github.com>
2022-01-10 17:29:01 +0900

    Update file_explorer.lua

--- UntitledOS/Programs/File-Manager/file_explorer.lua
+++ UntitledOS/Programs/File-Manager/file_explorer.lua
@@ -1,35 +1,36 @@
+local GUI = require("GUI")
 local shell = require("shell")
 local io = require("io")
 local os = require("os")
-local term = require("term")
 
+--------------------------------------------------------------------------------
 
-while true do
-  print("Select the operation you want to perform (ls, edit, mkdir, exit, rm)?")
+local workspace = GUI.workspace()
+workspace:addChild(GUI.panel(1, 1, workspace.width, workspace.height, 0x2D2D2D))
+
+local filesystemDialog = GUI.addFilesystemDialog(workspace, false, 50, math.floor(workspace.height * 0.8), "Open", "Cancel", "File name", "/")
+filesystemDialog:setMode(GUI.IO_MODE_OPEN, GUI.IO_MODE_FILE)
+filesystemDialog.onSubmit = function(path)
+  print("Select with edit, exit, rm.")
   operation = io.read()
-  if operation == "ls" then
-    print("Which directories to list?")
-    list_directories = io.read()
-    shell.execute("ls " .. list_directories)
-    print("done")
-  end
   if operation == "edit" then
     print("Which file to edit?")
     edit_files = io.read()
-    shell.execute("edit " .. edit_files)
-  end
-  if operation == "mkdir" then
-    print("Please enter the name of the folder.")
-    makedir = io.read()
-    shell.execute("mkdir " .. makedir)
+    shell.execute("edit " .. path)
   end
   if operation == "rm" then
-    print("Please enter the name of the folder.")
-    removedir = io.read()
-    shell.execute("rm " .. removedir)
+    shell.execute("rm " .. path)
   end
   if operation == "exit" then
     os.exit()
   end
   os.sleep(0)
 end
+
+filesystemDialog:show()
+
+--------------------------------------------------------------------------------
+
+workspace:draw()
+workspace:start()
+
